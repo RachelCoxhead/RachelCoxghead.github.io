c8070de140ad903760f0fef6d149158a51df0b9d
Ithrendrial <nsx9698@autuni.ac.nz>
2024-06-18 17:40:03 +1200

    resume download

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const onButtonClick = () => {
+    const pdfUrl = "RachelCoxhead_Resume.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "RachelCoxhead_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
   return (
     <div className="App">
         <p>Rachel Coxhead</p>
+        <button onClick={onButtonClick}>
+                    Download My Resume!
+                </button>
     </div>
   );
 }
