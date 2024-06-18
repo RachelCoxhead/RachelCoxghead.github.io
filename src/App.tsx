0daf6e877a053f26afa65df14a6b4a30eb106aa2
Ithrendrial <nsx9698@autuni.ac.nz>
2024-06-18 17:56:27 +1200

    add website buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Url } from 'url';
 
 function App() {
   const onButtonClick = () => {
@@ -12,12 +13,16 @@ function App() {
     document.body.removeChild(link);
 };
 
+  const onLinkButtonClick = (url: string) => {
+      window.open(url, '_blank');
+  };
+
   return (
     <div className="App">
         <p>Rachel Coxhead</p>
-        <button onClick={onButtonClick}>
-                    Download My Resume!
-                </button>
+        <button onClick={onButtonClick}>Download My Resume!</button>
+        <button onClick={() => onLinkButtonClick("https://www.linkedin.com/in/rachel-coxhead/")}>LinkedIn</button>
+        <button onClick={() => onLinkButtonClick("https://github.com/Ithrendrial")}>Github</button>
     </div>
   );
 }
